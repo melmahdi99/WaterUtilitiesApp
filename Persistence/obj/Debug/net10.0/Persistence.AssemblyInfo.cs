809a7b665a976ef3e460c14ef5c1d4911ac8bb50
nkantor1235467 <nkantor@clarku.edu>
2026-05-31 17:38:14 -0400

    fixed issue with building the project by editing WaterUtilitiesApp.slnx

--- Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+379b54aaefc3592dcac2523c894f6bade0d9dabe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0323a649c761665cc084831fae0070cc69ee795d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
