8043f5878e94ac9fcbb6e2091d21ba6aa8410f3d
Gian Santiago <Santiago.gian@gmail.com>
2026-06-01 11:46:35 -0400

    customer changes

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc3f68567ee120a24be5eff0b9279d5b8610cd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e71b804df7d3372e9b5a14715599d618ea2772")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
