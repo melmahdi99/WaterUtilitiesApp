d1893d1b0f97025a55e90d9a46abb119f9602b03
Ashleigh Boyd <ashleighboyd@gmail.com>
2026-06-02 10:11:22 -0700

    migrationsandtestedendpoints

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+379b54aaefc3592dcac2523c894f6bade0d9dabe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ab58acbebe07db8b45efb7a5af0bf54e3d4964")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
