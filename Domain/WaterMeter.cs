87e9cf8d25a471ba7255a60dab70d1bcd603efc2
alay1313 <artjj09@gmail.com>
2026-06-02 08:24:31 -0700

    water meter seeding data

--- Domain/WaterMeter.cs
+++ Domain/WaterMeter.cs
@@ -5,28 +5,28 @@ namespace Domain;
 public class WaterMeter
 {
     public Guid Id { get; set; }
-    public MeterReading MeterReading { get; set; }
+    public decimal MeterReading { get; set; }
     public bool IsOnline { get; set; }
 
     //FK
     public Guid BuildingId { get; set; }
 }
 
-public class MeterReading
-{
-    public decimal Value { get; set; }
-    public DateTimeOffset RecordedAt { get; set; }
+// public class MeterReading
+// {
+//     public decimal Value { get; set; }
+//     public DateTimeOffset RecordedAt { get; set; }
 
 
 
-    public decimal CalculateConsumption(decimal previousValue)
-    {   //calculates how much water has been consumed since the last reading
-        if (Value < previousValue) //if current reading is greater than or equal to the previous reading it returns the difference, your actual consumption
-        {
-            return 0m; //if the current reading is less than or equal to zero than it returns 0. Water meters should never be negative
-        }
+//     public decimal CalculateConsumption(decimal previousValue)
+//     {   //calculates how much water has been consumed since the last reading
+//         if (Value < previousValue) //if current reading is greater than or equal to the previous reading it returns the difference, your actual consumption
+//         {
+//             return 0m; //if the current reading is less than or equal to zero than it returns 0. Water meters should never be negative
+//         }
     
-        return Value - previousValue;
-    }
+//         return Value - previousValue;
+//     }
 
-}
+// }
---
+++ Persistence/WaterMeterDbinitializer.cs
@@ -0,0 +1,43 @@
+using System;
+using Domain;
+namespace Persistence;
+
+public class WaterMeterDbinitializer
+{
+    public WaterMeterDbinitializer(AppDbContext context, Guid buildingIdMain, Guid buildingIdIndustrial)
+    {
+        context.Database.EnsureCreated();
+
+        
+
+        var meters = new[]
+        {
+            new WaterMeter
+            {
+                Id = Guid.Parse("30000000-0000-0000-0000-000000000001"),
+                MeterReading = 1250.750m,
+                IsOnline = true,
+                BuildingId = buildingIdMain
+            },
+
+            new WaterMeter
+            {
+                Id = Guid.Parse("30000000-0000-0000-0000-000000000002"),
+                MeterReading = 890.000m,
+                IsOnline = false,
+                BuildingId = buildingIdIndustrial
+            },
+
+            new WaterMeter
+            {
+                Id = Guid.Parse("30000000-0000-0000-0000-000000000003"),
+                MeterReading = 5420.333m,
+                IsOnline = true,
+                BuildingId = buildingIdMain
+            }
+        };
+
+        context.WaterMeters.AddRange(meters);
+        context.SaveChanges();
+    }
+}
