a0168ca90e896850bbfd46bb93e1295808b6fb82
nkantor1235467 <nkantor@clarku.edu>
2026-06-02 11:49:00 -0400

    created repo and repo interface

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14c2b0936f6860cc7a18b037b8d867fd4f6bb282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f90488b18483a3187992c06a2266ba5f5696a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
