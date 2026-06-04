10406ae04bcc3aef3e350cebdebc165b7e7b77aa
Mustafa Elmahdi <m.melmahdi1@gmail.com>
2026-06-04 15:51:42 -0700

    fixed path for main file in index.html

--- water-utilities-app/src/types/main.tsx
+++ water-utilities-app/src/types/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from '../components/Global/App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider } from 'react-router';
+import { router } from '../components/Global/Routes.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />  
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
