10ba0bc62f4534a01f166fb10fd838e12acec559
nkantor1235467 <nkantor@clarku.edu>
2026-06-04 17:01:20 -0400

    took screenshots of the dto and class

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d45dbaa33ef48d069e418f51ed908ab8fa9ad9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21e6fd5878a507dbae4cedcd0c4af28ed031f303")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
