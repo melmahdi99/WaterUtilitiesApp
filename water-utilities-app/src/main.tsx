f9db40b061785fc20ba12e9bc63bf82884a942c8
Mustafa Elmahdi <m.melmahdi1@gmail.com>
2026-06-04 15:58:40 -0700

    fixed path for main file in index.html

--- water-utilities-app/src/main.tsx
+++ water-utilities-app/src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Welcome from './components/Global/Welcome.tsx'
-import LoginForm from './components/Global/LoginForm.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import {ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router';
 import { router } from './components/Global/Routes.tsx';
 
