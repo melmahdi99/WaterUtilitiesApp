283340287853014e0b5d4b4d968473299de97cb5
Mark <siebmark@gmail.com>
2026-06-05 12:32:46 -0700

    pres2

--- API/UserDtos/UserDto.cs
+++ API/UserDtos/UserDto.cs
@@ -7,4 +7,5 @@ public class UserDto
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public required string Email { get; set; }
+    public required string Role { get; set; }
 }
